bd7d2c34b19fd2edb2131ddb8d7a9790c0b59ec6
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-14 11:35:59 -0600

    📅  | Tue Jun 14 11:35:59 MDT 2022

--- hello-commander/src/hello-commander/hello-commander.ts
+++ hello-commander/src/hello-commander/hello-commander.ts
@@ -1,18 +1,17 @@
-import { Command, CommandRunner } from 'nest-commander';
+import { Command, CommandRunner, Option } from 'nest-commander';
 
 @Command({ name: 'sayHello', options: { isDefault: true } })
 export class HelloCommander implements CommandRunner {
   async run(inputs: string[], options: { name: string; age: number }) {
     console.log(`Hello ${options.name} are you ${options.age} years old?`);
   }
-}
+  @Option({ flags: '-n <name>' })
+  parseName(val: string) {
+    return val;
+  }
 
-@Option({flags: '-n <name>'})
-parseName(val: string) {
-  return val
-}
-
-@Option({flags: '-a <age>'})
-parseAge(val: string) {
-  return Number(val)
+  @Option({ flags: '-a <age>' })
+  parseAge(val: string) {
+    return Number(val);
+  }
 }
