fbf94686181984e5e5f82cd90efe8d8a967a2246
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-06-14 11:21:37 -0600

    📅  | Tue Jun 14 11:21:37 MDT 2022

--- hello-commander/src/hello-commander/hello-commander.ts
+++ hello-commander/src/hello-commander/hello-commander.ts
@@ -1,1 +1,13 @@
-export class HelloCommander {}
+import { Command, CommandRunner } from 'nest-commander';
+
+@Command({ name: 'sayHello', options: { isDefault: true } })
+export class HelloCommander implements CommandRunner {
+  async run(inputs: string[], options: { name: string; age: number }) {
+    console.log(`Hello ${options.name} are you ${options.age} years old?`);
+  }
+}
+
+@Option({flags: '-n <name>'})
+parseName(val: string) {
+  return val
+}
